55a5470bc98ff98f28d1b6ac1e1fb88f93e1c104
vjking007 <Freelancing@97177>
2025-08-02 15:32:27 +0530

    first commit

---
+++ src/main/java/com/example/custom_query/CustomQueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.custom_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomQueryApplication.class, args);
+	}
+
+}
